68204cde250c1a10a2b113934cd7a88bc141ec40
shektor <44544977+shektor@users.noreply.github.com>
2019-05-27 22:23:49 +0100

    Add posts new controller action and view
    Allow the user to visit the new posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,6 @@ class PostsController < ApplicationController
   def index
     @user = User.find_by(id: session[:user_id])
   end
+
+  def new; end
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to render_template(:index)
     end
   end
+
+  describe 'GET #new' do
+    it 'returns a success response' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
 end
